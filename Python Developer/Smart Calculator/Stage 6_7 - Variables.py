63c99dd0cb5abea4c8dd97464134a8c1c441df19
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-12-22 10:33:07 +0000

    Update Stage 6_7 - Variables.py

--- Python Developer/Smart Calculator/Stage 6_7 - Variables.py
+++ Python Developer/Smart Calculator/Stage 6_7 - Variables.py
@@ -1,5 +1,5 @@
 variables = dict()
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 while True:
 
@@ -16,15 +16,16 @@ while True:
             print('This program will calculate the sum of a user'
                   'enter list of numbers separated by spaces.\n'
                   'It can calculate equations containing plus and minus only.')
+            continue
         else:
             print('Unknown command')
+            continue
     if '=' in i:
         # assigning new varible
         try:
             equ = i.split()
             equ = ''.join(equ)
             equ = equ.split('=')
-            print(equ)
 
             if len(equ) != 2:
                 # if len(equ) > 3 or len(equ) <= 1:
@@ -39,13 +40,21 @@ while True:
                 if char.lower() not in alphabet:
                     clean = False
                     break
+            for char in equ[-1]:
+                if char in alphabet:
+                    if equ[-1] not in variables.keys():
+                        raise Exception('Invalid assignment')
 
             if not clean:
                 raise Exception('Invalid identifier')
-            elif equ[0] in variables.keys():
-                raise Exception('Invalid identifier')
+            # elif equ[0] in variables.keys():
+            #     raise Exception('Invalid identifier')
             else:
-                variables[equ[0]] = float(equ[-1])
+                if equ[-1] in variables.keys():
+                    variables[equ[0]] = variables[equ[-1]]
+                else:
+                    variables[equ[0]] = equ[-1]
+
 
         except ValueError:
             print('Invalid assignment')
@@ -57,31 +66,47 @@ while True:
         try:
             equ = i.split()
 
-            print(equ)
-            # determine if variables used
-            if alphabet in equ:  # will not work, check if element in dictionary first!
-                # Variables used
-                for index, element in equ:
-                    print(element)
-                    if element in variables.keys():
-                        equ[index] = variables[element]  #  replace elements in equation with the correct
-                        # values, taking care of + mand minues
+            for index, element in enumerate(equ):
+                # if element in variables.keys():
+                #     equ[index] = variables[element]  # replace elements in equation with the correct
+                #     # values, taking care of + mand minues
+
+                for char in element:
+                    if char in alphabet and element not in variables.keys():
+                        raise Exception('Unknown variable')
+                if element in variables.keys():
+                    equ[index] = variables[element]
+
+
 
 
             if '-' in equ[0]:
-                # try:
-                sum = -float(equ[0][1:])
-                # except:
-                #     sum = float(equ[0][:-1])
+                try:
+                    sum = -float(equ[0][1:])
+                except:
+                    # try:
+                    #
+                    #
+                    #     t = equ[0].split('-')
+                    #
+                    #
+                    #     sum = float(t[0]) - float(t[1])
+                    # except:
+                        sum = -float(equ[0][:-1])
+
+                    # try:
+                    #     equ[1] = -equ[1]
+                    # except:
+                    #     pass
 
             elif '+' in equ[0]:
 
-                sum = float(equ[0][1:])
-                # except:
-                #     sum = float(equ[0][:-1])
+                try:
+                    sum = float(equ[0][1:])
 
+                except:
+                    sum = float(equ[0][:-1])
             else:
-
                 sum = float(equ[0])
 
             if len(equ) != 1:
@@ -96,7 +121,10 @@ while True:
 
             print(int(sum))
 
+        except Exception as e:
+            e = str(e)
 
-        except:
-            print('Invalid Expression')
-
+            if e == 'Unknown variable':
+                print('Unknown variable')
+            else:
+                print('Invalid Expression') 
