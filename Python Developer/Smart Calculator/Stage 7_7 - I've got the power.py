1d521d90640e6f5d2767f6d6260c6cd7cec1b2b2
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-12-26 05:56:24 +0000

    Update Stage 7_7 - I've got the power.py

--- Python Developer/Smart Calculator/Stage 7_7 - I've got the power.py
+++ Python Developer/Smart Calculator/Stage 7_7 - I've got the power.py
@@ -1,102 +1,150 @@
 from typing import List
+
 variables = dict()
 alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers = '1234567890'
 
-def calc(sum, equ, index):
-    if '+' in char:
-        sum += float(equ[index + 1])
-    if '-' in char and index != 0:
-        if len(char) % 2 == 0:
-            sum += float(equ[index + 1])
-        else:
-            sum -= float(equ[index + 1])
-    if '*' in char:
-        sum += float(equ[index + 1])
-    if '/' in char:
-        sum /= float(equ[index + 1])
-    return sum
 
-def is_negative(equ: List, index: int) -> bool:
-    if '-' not in str(equ[index]):
-        return False
-    else:
-        for i, char in enumerate(str(equ[index])):
-            if char != '-':
-                if (i) % 2 == 0:
-                    return False
+def bracket_scan(equ):
+    count = 0
+    solved = False
+
+    for open_index, ch in enumerate(equ):
+        if ch == '(':
+            if solved == True:
+                break
+            for closed_index, ch2 in enumerate(equ[open_index + 1:]):
+                if ch2 == '(':
+                    count += 1
+                if ch2 == ')':
+                    if count == 0:
+                        # print(f'{equ[open_index:closed_index +  2]} equ')
+                        # print(f'{[equ[open_index + 1:closed_index + 1]]} solver' )
+                        equ[open_index:closed_index + 2] = [solver(equ[open_index + 1:closed_index + 1])]
+                        # print(f'{equ} equ after')
+                        return equ
+                        if '(' not in equ:
+                            solved = True
+                        break
+                    else:
+                        count -= 1
                 else:
-                    return True
+                    if count != 0 and closed_index == len(equ) - 1:
+                        raise Exception('Unequal brackets')
 
-def mult_divide_scan()
-    should be a function used in calc
-    if * or / found mult/divide ones next to each other, retun
-    answers and +/-'s unreplaced to calc'
-
-def bracket_scan():
-    count = int
-    index_of_open, index_of_closed = [], []
-
-    for index, char in equ:
-        if char == '(':
-            index_of_open.append(index)
+    return equ
 
 
-
-    if count of open inside > 1
-        find more than one close
-    if bracket closed found send to calc
-        doing internal things first,
-send results and indices to replace to priority
-    return result and indicies to replace
-
-
-# def priority():
-#     b, d, m, a, s = [], [], [], [], []
-#     open_brackets, closed_brackets = []
-#     for i, char in enumerate(equ):
-#         if char == '(':
-#             bracket_scan(equ, i)
-#
-#             get bracket indices send to calculate function
-#             send next bracket with the previous bracket replaced by its result
-
-def convert(equ):
+def convert(equ, variables):
     symbols = '()*/+-='
-    for index, char in enumerate(equ):
-        if char not in symbols:
-            equ[index] = float(char)
+    for index, element in enumerate(equ):
+        if element not in symbols:
+            equ[index] = is_negative(equ, index, variables)
+    return equ
+
+
+def is_negative(equ: List, index: int, variables) -> float:
+    # add errors
+    plus_count = 0
+    if '-' not in str(equ[index]) and '+' not in str(equ[index]):
+        if str(equ[index]) not in variables.keys():
+            return float(equ[index])
+        else:
+            return float(variables[str(equ[index])])
+    else:
+        for i, element in enumerate(str(equ[index])):
+            minus_count = 0
+            for index, char in enumerate(element):
+                if char in '1234567890':
+                    break
+                elif len(element) == 0 and element not in variables.keys():
+                    return element
+                else:
+                    if element == '-':
+                        minus_count += 1
+                    elif element == '+':
+                        pass
+                    if index + 1 == len(element):
+                        if minus_count % 2 == 0:
+                            return '+'
+                        else:
+                            return '-'
+
+            if element != '-':
+                if element == '+':
+                    plus_count += 1
+                    continue
+                elif int(i - plus_count) % 2 == 0:
+                    if str(equ[index])[i:] in variables.keys():
+                        print('yes')
+                        return float(variables[str(equ[index])[i:]])
+                    else:
+                        return float(-str(equ[index])[i:])
+                else:
+                    if str(equ[index])[i:] in variables.keys():
+                        return float(-variables[str(equ[index])[i:]])
+                    else:
+                        return float(-str(equ[index])[i:])
 
 
 def solver(equ):
-    solved = False
-    while not solved:
-            if '(' in equ:
-                # send to bracket scanner
-                # bracket scanner then sends the primary bracket back
-                # and this returns the answer to replace that bracket with
-            if '*' in equ:
-                i = equ.index('*')
-                equ[i-1, i + 1] = equ[i-1] * equ[i+1]
-                continue
-            elif '/' in equ:
-                i = equ.index('/')
-                equ[i-1, i + 1] = equ[i-1] / equ[i+1]
-            elif '+' in equ:
-                i = equ.index('+')
-                equ[i-1, i + 1] = equ[i-1] + equ[i+1]
-            elif '-' in equ:
-                i = equ.index('-')
-                equ[i-1, i + 1] = equ[i-1] + equ[i+1]
+    print(equ)
+    while True:
+        if '(' in equ:
+            equ = bracket_scan(equ)
+            continue
+        elif '/' in equ and '(' not in equ:
+            i = equ.index('/')
+            equ[i - 1:i + 1] = [equ[i - 1] / equ[i + 1]]
+            continue
+        elif '*' in equ and '(' not in equ:
+            i = equ.index('*')
+            equ[i - 1:i + 1] = [equ[i - 1] * equ[i + 1]]
+            continue
+        elif '+' in equ and '(' not in equ:
+            i = equ.index('+')
+            equ[i - 1:i + 1] = [equ[i - 1] + equ[i + 1]]
+            continue
+        elif '-' in equ and '(' not in equ:
+            i = equ.index('-')
+            equ[i - 1:i + 1] = [equ[i - 1] - equ[i + 1]]
+            continue
+        else:
+            return equ[0]
+
+
+def define_variable(i, variables):
+    try:
+        equ = i.split()
+        equ = ''.join(equ)
+        equ = equ.split('=')
+
+        if len(equ) != 2:
+            raise Exception('Invalid assignment')
+
+        for char in equ[0]:
+            if char.lower() not in alphabet:
+                raise Exception('Invalid identifier')
+
+        for char in equ[-1]:
+            if char in alphabet:
+                if equ[-1] not in variables.keys():
+                    raise Exception('Invalid assignment')
+
+        else:
+            if equ[-1] in variables.keys():
+                variables[equ[0]] = variables[equ[-1]]
             else:
-                return equ[0]
+                variables[equ[0]] = equ[-1]
 
+        print(variables)
 
+        return variables
 
-
-
-def define_variable():
-
+    except ValueError:
+        print('Invalid assignment')
+    except Exception as e:
+        print(e)
 
 
 while True:
@@ -119,82 +167,19 @@ while True:
             print('Unknown command')
             continue
     if '=' in i:
-        try:
-            equ = i.split()
-            equ = ''.join(equ)
-            equ = equ.split('=')
-
-            if len(equ) != 2:
-                raise Exception('Invalid assignment')
-
-            clean = True
-
-            for char in equ[0]:
-                if char.lower() not in alphabet:
-                    clean = False
-                    break
-            for char in equ[-1]:
-                if char in alphabet:
-                    if equ[-1] not in variables.keys():
-                        raise Exception('Invalid assignment')
-
-            if not clean:
-                raise Exception('Invalid identifier')
-            else:
-                if equ[-1] in variables.keys():
-                    variables[equ[0]] = variables[equ[-1]]
-                else:
-                    variables[equ[0]] = equ[-1]
-
-
-        except ValueError:
-            print('Invalid assignment')
-        except Exception as e:
-            print(e)
+        variables = define_variable(i, variables)
 
     else:
 
-        try:
-            equ = i.split()
+        equ = i.split()
+        equ = convert(equ, variables)
+        print(solver(equ))
 
-            for index, element in enumerate(equ):
-
-                for char in element:
-                    if char in alphabet and element not in variables.keys():
-                        raise Exception('Unknown variable')
-                if element in variables.keys():
-                    equ[index] = variables[element]
-
-            if '-' in equ[0]:
-                try:
-                    sum = -float(equ[0][1:])
-                except:
-                        sum = -float(equ[0][:-1])
-
-            elif '+' in equ[0]:
-
-                try:
-                    sum = float(equ[0][1:])
-
-                except:
-                    sum = float(equ[0][:-1])
-            else:
-                sum = float(equ[0])
-
-            if len(equ) != 1:
-                for index, char in enumerate(equ):
-                    sum = calc(sum, equ, index)
-                    if '(' in char:
-                        pass
-
-
-            print(int(sum))
-
-        except Exception as e:
-            e = str(e)
-
-            if e == 'Unknown variable':
-                print('Unknown variable')
-            else:
-                print('Invalid Expression')
-
+        #
+        # except Exception as e:
+        #     e = str(e)
+        #
+        #     if e == 'Unknown variable':
+        #         print('Unknown variable')
+        #     else:
+        #         print('Invalid Expression')
