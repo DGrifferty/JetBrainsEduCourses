3795cea6bc0b07e1ea468a5f55a581ed32fba3bf
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-12-27 09:32:49 +0000

    Update Calc_2.py

--- Python Developer/Smart Calculator/Calc_2.py
+++ Python Developer/Smart Calculator/Calc_2.py
@@ -42,11 +42,16 @@ def check_input(user_input, variables):
     equ = user_input.split()
     conv_equ = []
     symbols = '^*+-/'
+    # av = assigned variable
+    # bv = bad variable - letters and numbers
+    # uav = unassigned variable (just letters)
+    # n = number
+    # s = symbol (not brackets or =)
 
     for element in equ:
         contains_number = False
         contains_letter = False
-        
+
         print(f'{element} element')
 
         if element in variables.keys():
@@ -61,49 +66,59 @@ def check_input(user_input, variables):
             conv_equ.append('s')
         else:
             for char in element:
-                print(f'{char}, char}')
+                print(f'{char}, char')
                 if char in numbers:
                     contains_number = True
                     if contains_letter:
-                        conv_equ.append('bav')
+                        conv_equ.append('bv')
                         break
                 elif char in alphabet:
                     contains_letter = True
                     if contains_number:
-                        conv_equ.append('bav')
+                        conv_equ.append('bv')
                         break
                 elif char in '+-':
                     if contains_letter or contains_number:
                         E = 'bad element - +- in middle of element'
                 else:
-                    E = 'bad element - unallowed element/ character'
+                    E = 'bad element - not allowed element/ character'
 
             if contains_number:
                 conv_equ.append('n')
             else:
                 conv_equ.append('uav')
 
-        if '=' in conv_equ:
+    if '=' in conv_equ:
 
-            # Errors - bad variable name left
-            # Bad equation right -  bad assignment
-            # More than one =
-            allowed_combs = [['uav', '=', 'n'], ['uav', '=', 'av']]
-            if conv_equ not in allowed_combs:
-                print(conv_equ)
-                if 'bav' == equ[0]:
-                    E = 'Invalid identifier'
-                elif 'bav' == equ[2]:
-                    E = 'Invalid assignment'
-                else:
-                    E = 'Invalid assignment'
-                return False, E
-
+        # Errors - bad variable name left
+        # Bad equation right -  bad assignment
+        # More than one =
+        allowed_combs = [['uav', '=', 'n'], ['uav', '=', 'av']]
+        if conv_equ not in allowed_combs:
+            print(conv_equ)
+            if 'bv' == equ[0]:
+                E = 'Invalid identifier'
+            elif 'bv' == equ[2]:
+                E = 'Invalid assignment'
             else:
-                print(conv_equ)
-                return True, None
+                E = 'Invalid assignment'
+            return False, E
 
+        else:
+            print(conv_equ)
+            return True, None
 
+    if 'uav' in conv_equ:
+        E = 'Unassigned variable used'
+        return False, E
+    if 'bv' in conv_equ:
+        E = 'Bv used'
+        return False, E
+    # if number not at start or end there should be symbols to left and right, or closed bracket to right
+
+    # if open bracket should have symbol to left and number to right, unless first
+    # should be same number of open brackets as closed brackets
+    # symbols should have numbers to right and left, or open brackets to right, or closed brackets to left
 
 
 
